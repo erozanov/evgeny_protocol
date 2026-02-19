8a9feb067b63f1a391d8152fe7855f647669e32d
erozanov <evgeny.rozanov@gmail.com>
2026-02-19 14:36:56 +0400

    feat: add absakovo-2025 image to portrait deck

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,16 @@ export default {
 			colors: {
 				black: '#000',
 				white: '#fff',
+				// Current config color
 				'brutal-red': '#E63E8D',
 				'brutal-blue': '#00a3ff',
 				'brutal-green': '#1F8D3D',
 				'brutal-yellow': '#F2C100',
+			},
+			// Добавим стандартную толщину обводки для всего
+			borderWidth: {
+				'3': '3px',
+				'4': '4px',
 			},
 			fontFamily: {
 				sans: ['system-ui', 'sans-serif'],
@@ -18,6 +24,12 @@ export default {
 			boxShadow: {
 				'brutal': '8px 8px 0px 0px #000',
 				'brutal-sm': '4px 4px 0px 0px #000',
+				// Акцентная тень для выделенных элементов (например, при клике)
+				'brutal-hover': '2px 2px 0px 0px #000',
+			},
+			// Добавляем transition для плавного "нажатия" кнопок
+			transitionProperty: {
+				'brutal': 'transform, box-shadow',
 			},
 			keyframes: {
 				scanline: {
